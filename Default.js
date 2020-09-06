7a5ac4aa9c1835e9d821ccde64b844520ff2e648
Toshi <dtkellogg10@gmail.com>
2020-09-05 17:08:27 -0700

    Putting possible heroku scripts in default.js as a placeholder

--- Default.js
+++ Default.js
@@ -6,6 +6,8 @@ function DefaultLayout(props) {
       <h1>Default</h1>
     {/* "heroku-postbuild": "cd NM2 && npm install && npm run build"    </html> */}
     {/* "NPM_CONFIG_PRODUCTION=false npm install && npm run build" */}
+        {/* "heroku-postbuild": "cd NM2 && npm install --only=dev && npm install && npm run build", */}
+        {/* "NPM_CONFIG_PRODUCTION=false npm install --prefix NM2 && npm run build --prefix NM2", */}
   );
 }
 
